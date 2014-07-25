44030467f48f675949db722a913737c0c61dc3e3
gorupria <battle_g5@yahoo.com>
2014-07-25 16:16:47 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Harvey::Application.config.secret_key_base = 'fa022541b2c8b9cae5023e287b43643930f06747c0b10608dc4a99af319ce308aa43275ea8ad004d6fe38ec6d60aa2aa21db671c61830ff4f337c06351b9f516'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Harvey::Application.config.session_store :cookie_store, key: '_Harvey_session'
